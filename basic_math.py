2f2cd34f6727314636c3d521db6efadd60da5f97
Christopher El-Murr <christopher.el-murr@stu.ocsb.ca>
2025-09-19 17:58:49 +0000

    updated basicmath.py

--- basic_math.py
+++ basic_math.py
@@ -5,16 +5,16 @@
 
 
 def main():
-    print("6+4={}". format(6+4))
-    print("6-2={}". format(6-2))
-    print("12*164={}". format(12*164))
-    print("6/4={}". format(6/4))
-    print("14/8={}". format(14/8))
-    print("6+4*9={}". format(6+4*9))
-    print("4^9={}". format(4^9)) #bitwise operator
-    print("7**3={}". format(7**3)) #exponent
-    print("36**0.5={}". format(36**0.5)) #square root
-    
+    print("6+4={}".format(6 + 4))
+    print("6-2={}".format(6 - 2))
+    print("12*164={}".format(12 * 164))
+    print("6/4={}".format(6 / 4))
+    print("14/8={}".format(14 / 8))
+    print("6+4*9={}".format(6 + 4 * 9))
+    print("4^9={}".format(4 ^ 9))  # bitwise operator
+    print("7**3={}".format(7**3))  # exponent
+    print("36**0.5={}".format(36**0.5))  # square root
+
+
 if __name__ == "__main__":
     main()
-
