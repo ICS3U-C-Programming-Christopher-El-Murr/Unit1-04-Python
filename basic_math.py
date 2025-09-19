653127b88859aefdd16c6eae3f13f59e753323e8
Christopher El-Murr <christopher.el-murr@stu.ocsb.ca>
2025-09-19 17:57:39 +0000

    basicmath.py

---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created By: Christopher El-Murr
+# Date: 09 17, 2025
+# This program is a basic math calculator capable of solving basic math operations.
+
+
+def main():
+    print("6+4={}". format(6+4))
+    print("6-2={}". format(6-2))
+    print("12*164={}". format(12*164))
+    print("6/4={}". format(6/4))
+    print("14/8={}". format(14/8))
+    print("6+4*9={}". format(6+4*9))
+    print("4^9={}". format(4^9)) #bitwise operator
+    print("7**3={}". format(7**3)) #exponent
+    print("36**0.5={}". format(36**0.5)) #square root
+    
+if __name__ == "__main__":
+    main()
+
